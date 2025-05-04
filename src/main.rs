edde65cce6d560e2a968f5461db31c87fd85caf2
richard <rcheng1729@gmail.com>
2025-05-03 21:32:04 -0700

    [+] uhhhhhh

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("lol fuck my life");
+use std::fs::File;
+use std::io::{self, Error, Read};
+
+fn main() -> io::Result<()> {
+    let mut file = File::open("zzz.txt")?;
+    // let mut out = File::open("out.txt");
+    let mut buffer = [0u8; 1];
+    let mut warn = false;
+
+    'outer: while file.read(&mut buffer)? != 0 {
+        let byte = buffer[0];
+
+        if (byte >> 7) & 1 == 0 {
+            warn = true;
+            continue 'outer;
+        }
+
+        let mut length = 0;
+        for i in (4..=6).rev() {
+            if (byte >> i) & 1 == 0 {
+                break;
+            }
+            length += 1;
+        }
+
+        let shift = 2 + length;
+        let mut cur = (byte << shift) >> shift;
+
+        for _ in 0..length {
+            if file.read(&mut buffer)? == 0 {
+                break;
+            }
+            let byte = buffer[0];
+            cur <<= 6;
+            cur += (byte << 2) >> 2;
+        }
+
+        println!("{}", cur);
+    }
+
+    if warn {
+        println!("Error occured while parsing")
+    }
+
+    Ok(())
 }
